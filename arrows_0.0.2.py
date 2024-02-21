acf6f9e49b281b4dc4805aec15a3fd7d79abf1bc
jessie-moe <jessie.moe@email.phoenix.edu>
2024-02-21 06:56:48 -0800

    Made a function, added another arrow

---
+++ arrows_0.0.2.py
@@ -0,0 +1,22 @@
+def one_arrow():   
+    print("    *")
+    print("   * *")
+    print("  *   *")
+    print(" *     *")
+    print("***   ***")
+    print("  *   *")
+    print("  *   *")
+    print("  *****")
+    
+def two_arrows():
+    print("    *            " * 2)
+    print("   * *           " * 2)
+    print("  *   *          " * 2)
+    print(" *     *         " * 2)
+    print("***   ***        " * 2)
+    print("  *   *          " * 2)
+    print("  *   *          " * 2)
+    print("  *****          " * 2)
+    
+one_arrow()
+two_arrows()
